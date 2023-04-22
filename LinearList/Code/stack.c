2b4a98e81efdd859fae57df89b7f2cfdd3e044da
SORmaker <1583211717@qq.com>
2023-04-23 00:30:26 +0800

    add LinearList base code
    完成线性表课本上所有基本操作
    将线性表操作分离出main.c文件，独立成linearlist文件
    将部分线性表操作思路写到linearlist.md文件，其余操作大同小异不再添加

---
+++ LinearList/Code/stack.c
@@ -0,0 +1,7 @@
+//
+// Created by sorrymaker on 2023/4/22.
+//
+
+#include "stack.h"
+
+
---
+++ LinearList/Code/stack.h
@@ -0,0 +1,8 @@
+//
+// Created by sorrymaker on 2023/4/22.
+//
+
+#ifndef CODE_STACK_H
+#define CODE_STACK_H
+
+#endif //CODE_STACK_H
